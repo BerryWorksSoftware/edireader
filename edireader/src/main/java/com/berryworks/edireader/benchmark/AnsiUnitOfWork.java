ac99eed096611d7a386b782d1866255260fa11aa
Scott Mayberry <scott@canabrook.org>
2015-04-29 20:51:59 -0500

    Update to version 5.3.2

--- edireader/src/main/java/com/berryworks/edireader/benchmark/AnsiUnitOfWork.java
+++ edireader/src/main/java/com/berryworks/edireader/benchmark/AnsiUnitOfWork.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2015 by BerryWorks Software, LLC. All rights reserved.
+ * Copyright 2005-2011 by BerryWorks Software, LLC. All rights reserved.
  *
  * This file is part of EDIReader. You may obtain a license for its use directly from
  * BerryWorks Software, and you may also choose to use this software under the terms of the
@@ -22,8 +22,6 @@ package com.berryworks.edireader.benchmark;
 
 import com.berryworks.edireader.EDIReader;
 import com.berryworks.edireader.EDIReaderFactory;
-import com.berryworks.jquantify.EventCounter;
-import com.berryworks.jquantify.SessionCounter;
 import org.xml.sax.InputSource;
 import org.xml.sax.helpers.DefaultHandler;
 
@@ -31,78 +29,35 @@ import java.io.PipedReader;
 import java.io.PipedWriter;
 import java.io.Writer;
 
-public class AnsiUnitOfWork implements BenchmarkUnitOfWork
-{
+public class AnsiUnitOfWork extends BenchmarkUnitOfWork {
 
-  private SessionCounter sessionCounter;
+    @Override
+    public void run() {
+        if (getSessionCounter() == null)
+            throw new RuntimeException("SessionCounter net set");
 
-  private EventCounter charCounter;
-
-  private int interchangeKCs;
-
-  private int charCount;
-
-  public void run()
-  {
-    if (sessionCounter == null)
-      throw new RuntimeException("SessionCounter net set");
-
-    PipedReader pipedReader;
-    PipedWriter pipedWriter = new PipedWriter();
-    try
-    {
-      pipedReader = new PipedReader(pipedWriter);
-      generateContent(pipedWriter);
-      InputSource inputSource = new InputSource(pipedReader);
-      EDIReader reader = EDIReaderFactory.createEDIReader(inputSource);
-      reader.setContentHandler(new DefaultHandler());
-      reader.parse(inputSource);
-      charCount = reader.getTokenizer().getCharCount();
-      charCounter.add(charCount);
-    } catch (Exception e)
-    {
-      throw new RuntimeException("caught exception", e);
+        PipedReader pipedReader;
+        PipedWriter pipedWriter = new PipedWriter();
+        try {
+            pipedReader = new PipedReader(pipedWriter);
+            generateContent(pipedWriter);
+            InputSource inputSource = new InputSource(pipedReader);
+            EDIReader reader = EDIReaderFactory.createEDIReader(inputSource);
+            reader.setContentHandler(new DefaultHandler());
+            reader.parse(inputSource);
+            int charCount = reader.getTokenizer().getCharCount();
+            getCharCounter().add(charCount);
+        } catch (Exception e) {
+            throw new RuntimeException("caught exception", e);
+        }
+        getSessionCounter().stop();
     }
-    sessionCounter.stop();
-  }
 
-  private void generateContent(Writer writer)
-  {
-    EDITestData generator = new EDITestData();
-    generator.setWriter(writer);
-    generator.setInterchangeKCs(interchangeKCs);
-    new Thread(generator).start();
-  }
-
-  public int getCharCount()
-  {
-    return charCount;
-  }
-
-  public SessionCounter getSessionCounter()
-  {
-    return sessionCounter;
-  }
-
-  public void setSessionCounter(SessionCounter sessionCounter)
-  {
-    this.sessionCounter = sessionCounter;
-  }
-
-  public EventCounter getEventCounter()
-  {
-    return charCounter;
-  }
-
-  public void setCharCounter(EventCounter charCounter)
-  {
-    this.charCounter = charCounter;
-  }
-
-  public void setInterchangeKCs(int kcs)
-  {
-    interchangeKCs = kcs;
-  }
-
+    private void generateContent(Writer writer) {
+        EDITestData generator = new EDITestData();
+        generator.setWriter(writer);
+        generator.setInterchangeKCs(getInterchangeKCs());
+        new Thread(generator).start();
+    }
 
 }
