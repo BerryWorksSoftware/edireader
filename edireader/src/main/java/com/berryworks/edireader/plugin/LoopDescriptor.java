13a1bb0dba3fbddc9ccb8644dc2b5da4267d84b0
Scott Mayberry <scott@canabrook.org>
2015-05-06 08:38:30 -0500

    Update to version 5.3.2

--- edireader/src/main/java/com/berryworks/edireader/plugin/LoopDescriptor.java
+++ edireader/src/main/java/com/berryworks/edireader/plugin/LoopDescriptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2011 by BerryWorks Software, LLC. All rights reserved.
+ * Copyright 2005-2015 by BerryWorks Software, LLC. All rights reserved.
  *
  * This file is part of EDIReader. You may obtain a license for its use directly from
  * BerryWorks Software, and you may also choose to use this software under the terms of the
--- edireader/src/main/java/com/berryworks/edireader/tokenizer/TokenImpl.java
+++ edireader/src/main/java/com/berryworks/edireader/tokenizer/TokenImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2011 by BerryWorks Software, LLC. All rights reserved.
+ * Copyright 2005-2015 by BerryWorks Software, LLC. All rights reserved.
  */
 
 package com.berryworks.edireader.tokenizer;
