03dbcad467cbd7dbfcf225d755df274e3490fb85
Scott Mayberry <scott@canabrook.org>
2015-05-07 05:43:22 -0500

    Revert to edireader as the core artifcact name

--- edireader-community/src/main/java/com/berryworks/edireader/tokenizer/SourcePosition.java
+++
@@ -1,9 +0,0 @@
-package com.berryworks.edireader.tokenizer;
-
-public interface SourcePosition {
-    int getCharCount();
-
-    int getSegmentCharCount();
-
-    void setCharCounts(int charCount, int segmentCharCount);
-}
