a3a490c8946d8d5e1cada6f213ee452a10e33b6e
canabrook <p}#8JW[a'N_k'QV,XH>
2022-07-02 07:41:31 -0500

    Improve test cases for CONTRL messages

--- edireader/src/test/java/com/berryworks/edireader/EdifactCONTRLGeneratorTest.java
+++ edireader/src/test/java/com/berryworks/edireader/EdifactCONTRLGeneratorTest.java
@@ -38,4 +38,30 @@ public class EdifactCONTRLGeneratorTest {
         String actual = ackWriter.toString();
         assertEquals(expected, mask(actual, expected));
     }
+
+    @Test
+    public void canGenerateCONTRL_2messages_NoErrors() throws IOException, SAXException {
+
+        String ediText = EDITestData.getEdifactInterchange(2);
+        InputSource source = new InputSource(new StringReader(ediText));
+        EDIReader ediReader = EDIReaderFactory.createEDIReader(source);
+        ediReader.setContentHandler(new DefaultHandler());
+        StringWriter ackWriter = new StringWriter();
+        ediReader.setAckStream(new BranchingWriter(ackWriter));
+        ediReader.parse(source);
+
+        String expected = "UNB+IATA:1+REUAGT82AGENT/LHR01:PIMA+REUAIR08DLH:PIMA+??????:????+841F60UNZ'" +
+                "UNH+1+CONTRL:90:1:IA'" +
+                "UCI+841F60UNZ+REUAIR08DLH:PIMA+REUAGT82AGENT/LHR01:PIMA+7'" +
+                "UCM+1+DCQCKI:90:1:IA+7'" +
+                "UCM+1+DCQCKI:90:1:IA+7'" +
+                "UNT+5+1'" +
+                "UNZ+1+841F60UNZ'";
+        String actual = ackWriter.toString();
+        assertEquals(asLines(expected), asLines(mask(actual, expected)));
+    }
+
+    private String asLines(String wrappedSegments) {
+        return wrappedSegments.replace("'", "'\n");
+    }
 }
