8eabf1f4c4c0829b6d54bf7a136fa7575541dea2
canabrook <p}#8JW[a'N_k'QV,XH>
2025-09-09 06:48:00 +0200

    Introduce UN0E

--- src/test/java/com/berryworks/edireader/EdifactEncodingTest.java
+++ src/test/java/com/berryworks/edireader/EdifactEncodingTest.java
@@ -1,27 +1,42 @@
 package com.berryworks.edireader;
 
+import com.berryworks.edireader.util.sax.EDIReaderSAXAdapter;
+import org.junit.Before;
 import org.junit.Test;
+import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.StringReader;
 
+import static org.junit.Assert.assertEquals;
+
 public class EdifactEncodingTest {
 
     private EDIReader ediReader;
+    private MyContentHandler handler;
+
+    @Before
+    public void setUp() {
+        handler = new MyContentHandler();
+    }
 
     @Test
     public void baseline() throws IOException, SAXException {
         StringReader reader = new StringReader(EDIFACT_UNOA);
         ediReader = EDIReaderFactory.createEDIReader(reader);
+        ediReader.setContentHandler(handler);
         ediReader.parse(reader);
+        assertEquals("GENERAL WIDGET COMPANY", handler.getNad04());
     }
 
     @Test
     public void test_UNOE() throws IOException, SAXException {
         StringReader reader = new StringReader(EDIFACT_UNOE);
         ediReader = EDIReaderFactory.createEDIReader(reader);
+        ediReader.setContentHandler(handler);
         ediReader.parse(reader);
+        assertEquals("Рыба текст", handler.getNad04());
     }
 
     private static final String EDIFACT_UNOA = """
@@ -41,4 +56,25 @@ public class EdifactEncodingTest {
             UNT+4+00000000000117'
             UNZ+1+00000000000778'
             """;
+
+    private static class MyContentHandler extends EDIReaderSAXAdapter {
+
+        private boolean armed;
+        private String nad04;
+
+        @Override
+        protected void beginSegmentElement(Attributes atts) {
+            String id = atts.getValue("Id");
+            armed = "NAD04".equals(id);
+        }
+
+        @Override
+        protected void endSegmentElement(String value) {
+            if (armed) nad04 = value;
+        }
+
+        public String getNad04() {
+            return nad04;
+        }
+    }
 }
